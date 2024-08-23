42c2b221fdb3223b8350afadd161c74b550d68f3
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-08-22 20:17:49 -0600

    Update Tarjeta.cs
    Nuevo Commit

--- CardHolder/Tarjeta.cs
+++ CardHolder/Tarjeta.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CardHolder
 {
-    internal class Tarjeta
+    internal class Tarjeta 
     {
         private static List<Tarjeta> listaTarjetas = new List<Tarjeta>();
         public Tarjeta(string numeroTarjeta, string fechaVencimiento, Cliente titularTarjeta, int cVV)
@@ -21,8 +21,6 @@ namespace CardHolder
 
         protected string NumeroTarjeta { get; set; }
         protected string FechaVencimiento { get; set; }
-        //Para ingresar fecha de vencimiento se piden dos numeros (try catch) y
-        //luego se unen en un string "mm / yy" si quieren lo hago luego
         protected Cliente TitularTarjeta { get; set; }
         protected int CVV { get; set; }
 
@@ -30,29 +28,75 @@ namespace CardHolder
         {
             Console.WriteLine("-----TARJETA-----");
             Console.WriteLine("\nNumero Tarjeta: " + NumeroTarjeta);
-            //Aqui siguen ustedes
+
         }
+        
         public void AgregarInformacionDebito()
         {
-            Console.Write("Ingrese Numero de Tarjeta: ");
-            string NuevoNumero = Console.ReadLine(); 
-            Console.Write("Ingrese Fecha de Vencimiento(MM/YY): ");
-            string NuevoVencimiento = Console.ReadLine();
-            Console.Write("Ingrese Nuevo CVV: ");
-            int NuevoCvv = Convert.ToInt32(Console.ReadLine);
-            Console.Write("Ingrese Nombre del titular de Tarjeta: ");
-            string NuevoNombre = Console.ReadLine();
-            Tarjeta NuevaTarjeta = new Tarjeta(NuevoNumero, NuevoVencimiento, NuevoCvv);
-            TarjetaDebito.Add(NuevaTarjeta);
-        }
+                        Console.Write("Ingrese Número de Tarjeta: ");
+            string numeroTarjeta = Console.ReadLine();
+
+            Console.Write("Ingrese Fecha de Vencimiento (MM/YY): ");
+            string fechaVencimiento = Console.ReadLine();
+
+            Console.Write("Ingrese CVV: ");
+            int cVV = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("¿La tarjeta tiene seguro? (si/no): ");
+            bool tieneSeguro = Convert.ToBoolean(Console.ReadLine());
+
+            TarjetaDebito NuevaTarjetaDebito = new TarjetaDebito(numeroTarjeta, fechaVencimiento, titularTarjeta, cVV, tieneSeguro);
+            listaTarjetas.Add(NuevaTarjetaDebito);
+                    }
+        public  void AgregarInformacionCredito()
+        {
+            Console.Write("Ingrese Número de Tarjeta: ");
+            string numeroTarjeta = Console.ReadLine();
+
+            Console.Write("Ingrese Fecha de Vencimiento (MM/YY): ");
+            string fechaVencimiento = Console.ReadLine();
+
+            Console.Write("Ingrese Nombre del Titular de la Tarjeta: ");
+            string nombreTitular = Console.ReadLine();
+
+            Console.Write("Ingrese CVV: ");
+            int cVV = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese Límite de Crédito: ");
+            double limiteCredito = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese Saldo Utilizado: ");
+            double saldoUtilizado = Convert.ToDouble(Console.ReadLine());
+
+            TarjetaCredito NuevaTarjetaCredito = new TarjetaCredito(numeroTarjeta, fechaVencimiento, titularTarjeta, cVV, limiteCredito, saldoUtilizado);
+            listaTarjetas.Add(NuevaTarjetaCredito);
+        }        
         public void BuscarInformacion()
         {
+            bool Encontrado = false;
+Console.Clear();
+Console.WriteLine("------------------------");
+Console.WriteLine("     BUSCAR TARJETA");
+Console.WriteLine("------------------------\n");
+Console.Write("Número de cuenta: ");
+string NumeroTarjeta = Console.ReadLine();
 
+foreach (var Buscando in listaTarjetas)
+{
+    if (Buscando.NumeroTarjeta == NumeroTarjeta)
+    {
+        Console.WriteLine("Cliente Encontrado");
+        Buscando.MostrarInformacion();
+        Encontrado = true;
+
+    }
+}
+if (!Encontrado)
+{
+    Console.WriteLine("Cliente no encontrado...");
+}
 
         }
-        public void EliminarInformacion()
-        {
-
-        }
+      
     }
 }
